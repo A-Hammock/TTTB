8ca3d32b5bd3afe9f6dac0b1f6710605b0b98fe4
A. Hamm <102124298+A-Hammock@users.noreply.github.com>
2024-01-01 00:05:34 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,85 +1,93 @@
-const PLAYER_X_CLASS = "tk.png";
-const PLAYER_O_CLASS = "cb.png";
-const WINNING_COMBINATIONS = [
-	[0, 1, 2],
-	[3, 4, 5],
-	[6, 7, 8],
-	[0, 3, 6],
-	[1, 4, 7],
-	[2, 5, 8],
-	[0, 4, 8],
-	[2, 4, 6]
-]
-const cellElements = document.querySelectorAll('[data-cell]')
-const boardElement = document.getElementById('board')
-const winningMessageElement = document.getElementById('winningMessage')
-const restartButton = document.getElementById('restartButton')
-const winningMessageTextElement = document.getElementById('winningMessageText')
-let isPlayer_O_Turn = false
+var markers = [
+  <img src= “https://i.postimg.cc/5y7dYZ3N/tk.png”/>,
+  <img src= “https://i.postimg.cc/5y7dYZ3N/tk.png”/>
+];
+//var players = [];
+var players = [“Bro Exotic”, “Carolla Baskin Robbins”]; 
+var totals = [];
+var winCodes = [7,56,73,84,146,273,292,448];
+var whoseTurn = 0;
+var gameOver = false;
+var speed = [2, 2];
 
-startGame()
 
-restartButton.addEventListener('click', startGame)
-
-function startGame() {
-	isPlayer_O_Turn = false
-	cellElements.forEach(cell => {
-		cell.classList.remove(PLAYER_X_CLASS)
-		cell.classList.remove(PLAYER_O_CLASS)
-		cell.removeEventListener('click', handleCellClick)
-		cell.addEventListener('click', handleCellClick, { once: true })
-	})
-	setBoardHoverClass()
-	winningMessageElement.classList.remove('show')
+// Play again button
+function restartGame()
+{
+	
+	startGame();
 }
-function handleCellClick(e) {
-	const cell = e.target
-	const currentClass = isPlayer_O_Turn ? PLAYER_O_CLASS : PLAYER_X_CLASS
-	placeMark(cell, currentClass)
-	if (checkWin(currentClass)) {
-		endGame(false)
-	} else if (isDraw()) {
-		endGame(true)
-	} else {
-		swapTurns()
-		setBoardHoverClass()
+function startGame() // Choose your names, and display them in the header message.
+{
+	// makes board spin initially
+	document.getElementById("game-board").style.animation = "rotation 4s infinite linear";
+	//players[0] = prompt("Player 1 NAME:");
+	//players[1] = prompt("Player 2 NAME:");
+	var counter = 1;
+	var innerDivs = "";
+	for (i = 1; i <=3; i++)
+	{
+		innerDivs += '<div id="row-' + i + '">';
+		
+		for (j = 1; j <= 3; j++)
+		{
+			innerDivs += '<div onclick="playGame(this,' + counter + ');"></div>';
+			counter *= 2;
+		}
+		innerDivs += '</div>';
+	}
+	
+	document.getElementById("game-board").innerHTML = innerDivs;
+	totals = [0, 0];
+	gameOver = false;
+	document.getElementById("game-message").innerText = "IT'S YOUR TURN " + players[whoseTurn];
+}
+
+function playGame(clickedDiv, divValue)
+{
+	if (!gameOver)
+	{	
+	// changes speed depending on how many turns each player has done
+	speed[whoseTurn]++;
+	document.getElementById("game-board").style.animation = "rotation "+  8 / speed[whoseTurn] + "s infinite linear";
+	
+		// Adds X or O depending on whoseTurn
+		clickedDiv.innerHTML = markers[whoseTurn]; 
+		
+		// adds up total each time a player "moves" to track a win condition
+		totals[whoseTurn] += divValue;
+		
+		// calls isWin() function to see if someone won
+		if (isWin())
+		{
+			document.getElementById("game-message").innerText = "WOW VERY COOL " + players[whoseTurn] + " YOU WON";
+			document.getElementById("game-board").style.animation = "slide 2s forwards, rotation "+  8 / speed[whoseTurn] + "s infinite linear";
+		}
+		else if (gameOver)
+		{
+			document.getElementById("game-message").innerText = "YOU BOTH FAILED";
+		}
+		else
+		{
+		// Switches turn each click
+		if (whoseTurn) whoseTurn = 0; else whoseTurn = 1; 
+		
+		// disables onclick tag after clicked so it cannot be used >1 times.
+		clickedDiv.attributes["0"].nodeValue = ""; 
+		
+		// Displays text for which turn it is
+		document.getElementById("game-message").innerText = "IT'S YOUR TURN " + players[whoseTurn];
+		}
 	}
 }
-function endGame() {
-  if (draw) {
-    winningMessageTextElement.innerText = "Draw!!"
-  } else {
-    winningMessageTextElement.innerText = 'Player with ${isPlayer_O_Turn ? "O's' : "X's"} wins!'
-  }
-  winningMessageElement.classList.add('show')
-  }
-  
-function isDraw() {
-	return [...cellElements].every(cell => {
-		return cell.classList.contains(PLAYER_X_CLASS) || cell.classList.contains(PLAYER_O_CLASS)
-	})
-}
-function placeMark(cell, currentClass) {
-	cell.classList.add(currentClass)
-}
 
-function swapTurns() {
-	isPlayer_O_Turn = !isPlayer_O_Turn
-}
-function setBoardHoverClass() {
-	boardElement.classList.remove(PLAYER_X_CLASS)
-	boardElement.classList.remove(PLAYER_O_CLASS)
-	if (isPlayer_O_Turn) {
-		boardElement.classList.add(PLAYER_O_CLASS)
-	} else {
-		boardElement.classList.add(PLAYER_X_CLASS)
+// win code logic
+function isWin()
+{
+	for (i = 0; i < winCodes.length; i++)
+	{
+		if ((totals[whoseTurn] & winCodes[i]) == winCodes[i]) {gameOver = true;  return true;}
 	}
-}
-function checkWin(currentClass) {
-	return WINNING_COMBINATIONS.some(combination => {
-		return combination.every(index => {
-			return cellElements[index].classList.contains(currentClass)
-		})
-	})
-}
-</script>
+	if (totals[0] + totals[1] == 511) {gameOver = true;}
+	
+	return false;
