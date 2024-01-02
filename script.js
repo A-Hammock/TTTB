bb5b786c0d595f6c144fa743e2ef607e61a400d9
A. Hamm <102124298+A-Hammock@users.noreply.github.com>
2024-01-02 09:55:54 -0500

    Update script.js

--- script.js
+++ script.js
@@ -87,7 +87,7 @@ btnRef.forEach((element) => {
     if (xTurn) {
       xTurn = false;
       //Display X
-      element.innerHTML = "<img src= https://i.postimg.cc/WzWbV1br/Untitled-design-1.png id=button-options/>";
+      element.innerHTML = "<img src= https://i.postimg.cc/zGMRm5x4/Untitled-design-2.png id=button-options/>";
       element.disabled = true;
     } else {
       xTurn = true;
